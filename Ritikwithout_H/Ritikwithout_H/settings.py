1b24143fac5ab0bdb61f395637073b8cbe4d8b99
whyme-duh <ritikshrestha94@gmail.com>
2022-12-28 12:38:24 +0545

    final update

--- Ritikwithout_H/Ritikwithout_H/settings.py
+++ Ritikwithout_H/Ritikwithout_H/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_SETTINGS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '192.168.1.66:5000']
 
